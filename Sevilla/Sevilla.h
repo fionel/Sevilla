ccbf39f93c3668e49376ea89bb03e1e7de547e07
Chan Darren <chan_darren@cyberagent.co.jp>
2016-10-11 12:43:38 +0900

    initial commit

---
+++ Sevilla/Sevilla.h
@@ -0,0 +1,19 @@
+//
+//  Sevilla.h
+//  Sevilla
+//
+//  Created by A12555 on 2016/10/06.
+//  Copyright © 2016年 CA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sevilla.
+FOUNDATION_EXPORT double SevillaVersionNumber;
+
+//! Project version string for Sevilla.
+FOUNDATION_EXPORT const unsigned char SevillaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sevilla/PublicHeader.h>
+
+
---
+++ Sevilla/Sevilla.swift
@@ -0,0 +1,18 @@
+//
+//  Sevilla.swift
+//  Sevilla
+//
+//  Created by A12555 on 2016/10/06.
+//  Copyright © 2016年 CA. All rights reserved.
+//
+
+import Firebase
+import FirebaseMessaging
+import FirebaseInstanceID
+
+public class Sevilla {
+    public func configure() {
+        FIRApp.configure()
+    }
+}
+
